aa6c93e348fd913fe0a7a0c44b93874a6e5ebeb5
Mariia <whitesheepa@gmail.com>
2021-07-11 22:49:57 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Stars from './Stars';
+import './App.css';
+
+function App() {
+
+  return (
+    <Stars count={5}/>
+  );
+   
+}
+
+export default App;
---
+++ src/Stars/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Star from "../Star";
+import uuid from "react-uuid";
+
+export default function Stars({ count }) {
+  let arr = [];
+
+  function myLoop() {
+    while (count > 0) {
+      arr.push("star");
+      count--;
+    }
+  }
+
+  return (
+    <div className="container">
+      {myLoop()}
+      <ul className="row" style={{ display: "flex" }}>
+        {arr.map((item) => (
+          <li style={{ listStyle: "none" }} key={uuid()} className="col">
+            <Star />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Stars.propTypes = {
+  count: PropTypes.number,
+};
+
+Stars.defaultProps = {
+  count: 1,
+};
